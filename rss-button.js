6da293b734d6874ef05a586970813caa7291de89
bluetinge <68611961+bluetinge@users.noreply.github.com>
2024-09-24 14:25:19 -0400

    added aync button script

---
+++ rss-button.js
@@ -0,0 +1,9 @@
+// load button stylesheet
+
+document.head.insertAdjacentHTML(
+    'beforeend',
+    '<link rel="stylesheet" href="https://rssr.bluetinge.dev/rss-button.css" />');
+    
+// eventually i'll add code to make the rss button pop up in an iframe instead of making a new tab
+    
+    
